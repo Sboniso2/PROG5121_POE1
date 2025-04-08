5870d2917c1de08f5a5dd6624b78f845814b8d1a
RC_Student_lab <RC_Student_lab@10.0.0.74>
2025-04-08 19:57:19 +0000

    code

---
+++ src/java_prog5121_poe/Java_Prog5121_Poe.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_prog5121_poe;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Java_Prog5121_Poe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
